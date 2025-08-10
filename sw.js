89ac4fc8b64ea2185f8b519bc0ef630be4ca978c
Ozan Meral <ozan.meral@profeed.app>
2025-08-10 23:48:32 +0300

    init

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'quran-reader-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './data/quranix_yasarnuri_simple.json'
+];
+
+// Install: cache app shell + data
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// Activate: cleanup old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first for JSON, cache-first for shell
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('.json')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request))
+    );
+  }
+});
